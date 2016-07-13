63b12fbecdc64c63f805a95557e155c6d3fed979
a1 <353658617@qq.com>
2016-07-13 16:08:56 +0800

    one

---
+++ src/com/company/HelloWord.java
@@ -0,0 +1,26 @@
+package com.company;
+
+/**
+ * 循环数组中的元素
+ * Created by a1 on 16/7/13.
+ */
+public class HelloWord {
+
+    public static void main(String[] args) {
+
+        // 定义一个长度为 3 的字符串数组，并赋值初始值
+        String[] hobbys = {"sports", "game", "movie"};
+        System.out.println("循环输出数组中元素的值：");
+
+        // 使用循环遍历数组中的元素
+
+        for (int i = 0; i < hobbys.length; i++) {
+            System.out.println(hobbys[i]);
+        }
+
+
+    }
+}
+
+
+
